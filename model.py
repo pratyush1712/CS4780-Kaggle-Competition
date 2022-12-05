2561fb31d35a9cba5cad3a043a77b4a1ed7c838f
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-12-05 03:02:23 -0500

    requirements

---
+++ model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+training_data = pd.read_csv("./train_data.csv")
+testing_data = pd.read_csv("./test_data_s.csv")
+print(training_data)
